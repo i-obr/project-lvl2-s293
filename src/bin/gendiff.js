e494a054704b80cf4e4e0ee42ef01c6a3d5c21e9
i-obr <obr.iv@ya.ru>
2018-07-22 18:25:44 +0300

    fix

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,12 +4,12 @@ import program from 'commander';
 import gendiff from '..';
 
 program
-  .version('1.3.3')
+  .version('1.3.4')
   .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
   .action((firstArg, secondArg) => {
-    console.log(gendiff(firstArg, secondArg));
+    console.log(gendiff(firstArg, secondArg, program.format));
   });
 
 program.parse(process.argv);
